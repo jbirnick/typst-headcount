7a8062bd72d2960bfeb416c58e345ee3b418800f
Johann Birnick <johann.birnick@hotmail.de>
2024-10-14 13:42:18 -0700

    initial

---
+++ build/render.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+render_file=$(echo $1_RENDER)
+output_file=$(echo "${1%.typ}.png")
+render_file1=$(echo $1_RENDER1)
+
+sed 's/"@preview\/heading-dependent-counters:0.1.0"/"lib.typ"/' $1 > $render_file1
+sed 's/"@preview\/great-theorems:0.1.0"/"great-theorems.typ"/' $render_file1 > $render_file
+
+typst compile \
+  --root . \
+  --input file="$render_file" \
+  build/render_template.typ \
+  "$output_file"
+
+rm $render_file
+rm $render_file1
---
+++ build/render_template.typ
@@ -0,0 +1,3 @@
+#set page(width: 40em, height: auto, margin: 1em)
+
+#include "/" + sys.inputs.file
